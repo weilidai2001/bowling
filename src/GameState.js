da41b91adfffcee7b342a757e4abf4de5f354598
Weili Dai <weili.dai@turner.com>
2017-03-25 13:49:11 +0000

    Use ES6 export

--- src/GameState.js
+++ src/GameState.js
@@ -1,6 +1,6 @@
 import {range} from 'lodash';
 
-const GameState = {
+export default {
     getInitialState(){
         return {
             frames: [
@@ -23,5 +23,3 @@ const GameState = {
         };
     }
 };
-
-module.exports = GameState;

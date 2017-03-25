d1503f523d63f4642feb95b3d883c82a61b4f6fa
Weili Dai <weili.dai@turner.com>
2017-03-25 19:44:20 +0000

    Added console input and output

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-import { range } from 'lodash';
+import readline from 'readline-sync';
+import GameState from './src/GameState';
+import GameLogic from './src/GameLogic';
+import OutputUtil from './src/OutputUtil';
 
+const outputDevice = {
+    print: console.log
+};
 
-console.log(range(10));
+const inputDevice = {
+    promptEnterScore: () => parseInt(readline.question('Please enter score for current bowl: '))
+};
+
+function start() {
+    let gameState = GameState.getInitialState();
+
+    continuePlayingUntilGameEnd(gameState);
+}
+
+function continuePlayingUntilGameEnd(gameState) {
+    if (!GameLogic.isGameEnded(gameState)) {
+        continuePlayingUntilGameEnd(bowl(gameState));
+    }
+}
+
+function bowl(state) {
+    const score = inputDevice.promptEnterScore();
+    const newState = GameLogic.calculateNewState(state, score);
+    outputDevice.print(OutputUtil.stringifyGameState(newState));
+    return newState;
+}
+
+start();

1e1257095a72d88df7f72951b72ab6b0727108f3
Weili Dai <weili.dai@turner.com>
2017-03-25 14:03:18 +0000

    Added failing test for helper function

--- test/GameState.spec.js
+++ test/GameState.spec.js
@@ -64,4 +64,68 @@ describe('GameState', function () {
             (GameState.getInitialState()).should.be.deepEqual(expectedState)
         });
     });
+
+    describe('addScoreForBall1()', function () {
+        it('should return a new state which has a score of 5 at frame 0 and ball 1', function () {
+            const initialState = GameState.getInitialState();
+            const expected = {
+                frames: [
+                    {
+                        ball1: '5',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        score: '?'
+                    },
+                    {
+                        ball1: '?',
+                        ball2: '?',
+                        bonusBall1: '?',
+                        bonusBall2: '?',
+                        score: '?'
+                    }
+                ]
+            };
+
+            (GameState.addScoreForBall1(initialState, 0, 5)).should.be.deepEqual(expected)
+        });
+    });
 });

2394e5f06b4413b36a0ceb4b3611f9d56d3a5ee2
Weili Dai <weili.dai@turner.com>
2017-03-25 14:36:09 +0000

    Added failing test for the scenario when game is complete

--- test/GameLogic.spec.js
+++ test/GameLogic.spec.js
@@ -7,7 +7,7 @@ describe('GameLogic', function () {
         it('should return false when game is in initial state', function () {
             const initialState = GameState.getInitialState();
 
-            (GameLogic.isGameEnded(initialState)).should.be.false();
+            GameLogic.isGameEnded(initialState).should.be.false();
         });
 
         it('should return false when game is in progress', function () {
@@ -68,7 +68,68 @@ describe('GameLogic', function () {
                 ]
             };
 
-            (GameLogic.isGameEnded(gameInProgressState)).should.be.false();
+            GameLogic.isGameEnded(gameInProgressState).should.be.false();
+        });
+
+        it('should return true when final frame has two balls played and there was no spare nor strike', function () {
+            const gameComplete = {
+                frames: [
+                    {
+                        ball1: 0,
+                        ball2: 5,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        bonusBall1: '?',
+                        bonusBall2: '?',
+                        score: '?'
+                    }
+                ]
+            };
+
+            GameLogic.isGameEnded(gameComplete).should.be.true();
         });
     });
 });

510d85567699d3a7b1d3b09acb672117c64b282c
Weili Dai <weili.dai@turner.com>
2017-03-25 14:46:51 +0000

    Added failing test

--- test/GameLogic.spec.js
+++ test/GameLogic.spec.js
@@ -131,5 +131,66 @@ describe('GameLogic', function () {
 
             GameLogic.isGameEnded(gameComplete).should.be.true();
         });
+
+        it('should return false when final frame has two balls played and there was a spare', function () {
+            const gameComplete = {
+                frames: [
+                    {
+                        ball1: 0,
+                        ball2: 5,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 2,
+                        score: '?'
+                    },
+                    {
+                        ball1: 3,
+                        ball2: 7,
+                        bonusBall1: '?',
+                        bonusBall2: '?',
+                        score: '?'
+                    }
+                ]
+            };
+
+            GameLogic.isGameEnded(gameComplete).should.be.false();
+        });
     });
 });
